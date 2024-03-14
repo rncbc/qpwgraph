c1515439b4971f1333d269e86079810f2fc18692
rncbc <rncbc@rncbc.org>
2024-03-14 22:12:32 +0000

    - Fixed system-tray icon to a 32x32 pixmap.

--- src/qpwgraph_systray.cpp
+++ src/qpwgraph_systray.cpp
@@ -1,7 +1,7 @@
 // qpwgraph_systray.cpp
 //
 /****************************************************************************
-   Copyright (C) 2021-2022, rncbc aka Rui Nuno Capela. All rights reserved.
+   Copyright (C) 2021-2024, rncbc aka Rui Nuno Capela. All rights reserved.
 
    This program is free software; you can redistribute it and/or
    modify it under the terms of the GNU General Public License
@@ -43,7 +43,7 @@ qpwgraph_systray::qpwgraph_systray ( qpwgraph_form *form )
 #if QT_VERSION < QT_VERSION_CHECK(6, 1, 0)
 	QSystemTrayIcon::setIcon(QIcon(":/images/qpwgraph.png"));
 #else
-	QSystemTrayIcon::setIcon(m_form->windowIcon());
+	QSystemTrayIcon::setIcon(m_form->windowIcon().pixmap(32, 32));
 #endif
 	QSystemTrayIcon::setToolTip(m_form->windowTitle());
 
